aa486967e5f0e1d9ad15a608bac50d3f0bc598a3
uchiyamanet <uchiyamaynet@gmail.com>
2019-11-11 18:30:22 +0900

    Add:model＆controller3

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
 
 # Rails.application.routes.draw do
