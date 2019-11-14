f4a04c18a12976ef093d099fe546887faf845d86
uchiyamanet <uchiyamaynet@gmail.com>
2019-11-14 17:38:26 +0900

    フォームが送信されたら、イベントが発火するようにする

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    console.log('こんにちは');
+  })
+})
+
+// $(function(){
+//   $('#new_message').on('submit', function(){
+//     console.log('こんにちは');
+//   })
+// })
