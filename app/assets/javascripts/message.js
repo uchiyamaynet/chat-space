1709b40ab03293f33676c91dc2ca1162bdc2d69b
uchiyamanet <uchiyamaynet@gmail.com>
2019-11-15 10:54:26 +0900

    messages#createでメッセージを保存し、respond_toを使用してHTMLとJSONの場合で処理を分ける

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,45 @@
 $(function(){
-  $('#new_message').on('submit', function(e){
-    e.preventDefault()
-    console.log('こんにちは');
+  $("#new_message").on('submit', function(e) {
+
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action');
+
+  $.ajax({
+  url: url,
+  type: "POST",
+  data: formData,
+  dataType: 'json',
+  processData: false,
+  contentType: false
+  
   })
+
 })
+})
+
+
+// $(function(){
+//   $('#new_message').on('submit', function(e){
+//     e.preventDefault()
+
+//     var formData = new FormData(this);
+//     var url = $(this).attr('action');
+
+//     $.ajax({
+      
+//       url: url,
+//       type: "POST",
+//       data: formData,
+//       dataType: 'json',
+//       processData: false,
+//       contentType: false
+    
+//       })
+//   })
+// })
+
+
 
 // $(function(){
 //   $('#new_message').on('submit', function(){
