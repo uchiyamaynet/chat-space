17a2f0612a4c6df154a14e0ec0886d26809e4792
uchiyamanet <uchiyamaynet@gmail.com>
2019-11-18 21:28:07 +0900

    表示されているメッセージのidが確認できるようにしよう

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,39 +1,138 @@
-$('#user-search-field').on('keyup', function(e){
-  var input = $("#user-search-field").val();
+$(function() {
+  function addUser(user) {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  }
 
-  $.ajax({
-      type: 'GET',                // HTTPメソッドはGETで
-      url:  '/users',             // /usersのURLに (これによりusersコントローラのindexアクションが起動)
-      data: { keyword: input},    // keyword: inputを送信する
-      dataType: 'json'            // サーバから値を返す際はjsonである
-  })
+  function addNoUser() {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+      .done(function(users) {
+        $("#user-search-result").empty();
 
-  .done(function(users){                // usersにjson形式のuser変数が代入される。複数形なので配列型で入ってくる
-
-      if (input.length === 0) {         // フォームの文字列長さが0であれば、インクリメンタルサーチ結果を表示しないようにする
-          $('#user-search-result').empty();
-        }
-
-      else if (input.length !== 0){     // 値が等しくないもしくは型が等しくなければtrueを返す。
-          $('#user-search-result').empty();
-          users.forEach(function(user){ // users情報をひとつずつとりだしてuserに代入
-              appendUser(user)
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            addUser(user);
           });
-      }
-
-      else {
-          $('#user-search-result').empty(); // ユーザーが見つからなければ「見つからない」を返す。
-          appendErrMsgToHTML("一致するユーザーが見つかりません");
-      }
-  })
-
-  .fail(function() {
-      alert('ユーザー検索に失敗しました');
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
+      })
+      .fail(function() {
+        alert("通信エラーです。ユーザーが表示できません。");
+      });
+  });
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    console.log
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this)
+      .parent()
+      .remove();
   });
 });
 
+
+
+
 // $(function() {
+//   function addUser(user) {
+//     let html = `
+//       <div class="chat-group-user clearfix">
+//         <p class="chat-group-user__name">${user.name}</p>
+//         <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+//       </div>
+//     `;
+//     $("#user-search-result").append(html);
+//   }
+
+//   function addNoUser() {
+//     let html = `
+//       <div class="chat-group-user clearfix">
+//         <p class="chat-group-user__name">ユーザーが見つかりません</p>
+//       </div>
+//     `;
+//     $("#user-search-result").append(html);
+//   }
+//   function addDeleteUser(name, id) {
+//     let html = `
+//     <div class="ChatMember clearfix" id="${id}">
+//       <p class="ChatMember__name">${name}</p>
+//       <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
+//     </div>`;
+//     $(".ChatMembers").append(html);
+//   }
 //   $("#user-search-field").on("keyup", function() {
+//     let input = $("#user-search-field").val();
+//     $.ajax({
+//       type: "GET",
+//       url: "/users",
+//       data: { keyword: input },
+//       dataType: "json"
+//     })
+//       .done(function(users) {
+//         $("#user-search-result").empty();
+
+//         if (users.length !== 0) {
+//           users.forEach(function(user) {
+//             addUser(user);
+//           });
+//         } else if (input.length == 0) {
+//           return false;
+//         } else {
+//           addNoUser();
+//         }
+//       })
+//       .fail(function() {
+//         alert("通信エラーです。ユーザーが表示できません。");
+//       });
+//   });
+// });
+
+
+
+// $(function() {
+
+//     $("#user-search-field").on("keyup", function() {
 //     let input = $("#user-search-field").val();
 //     $.ajax({
 //       type: "GET",
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  # root 'groups#index'
-  root 'users#index'
+  root 'groups#index'
+  # root 'users#index'
   resources :users  , only:[:index,:edit,:update,:show]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
